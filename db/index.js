2c5d23316a5403e4506de4b9a24c073c68d6c831
Nicole <92593875+ozzleme@users.noreply.github.com>
2023-02-10 02:05:47 +0530

    updated

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb+srv://labarr-bell:1234@cluster0.jefqq6s.mongodb.net/test";
+  process.env.MONGODB_URI || "mongodb+srv://labarrbell:1234@cluster0.jefqq6s.mongodb.net/travelapp2?retryWrites=true&w=majority";
 
 mongoose
   .connect(MONGO_URI)
